d1475202812bbf44811c44ca837611873e7529b9
Camilo <arqcamilocardenasmesa@gmail.com>
2023-08-25 17:30:27 -0500

    chore(comment): state object

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -10,12 +10,14 @@ const usersSlice = createSlice({
    },
    extraReducers(builder) {
         builder.addCase(fetchUsers.pending, (state, action) => {
-
+            /** actualizar nuestro objeto de estado para mostrar 
+             * al usuario al usuario que estamos cargando datos
+             */
         }); 
         builder.addCase(fetchUsers.fulfilled, (state, action) => {
 
         }); 
-        builder.addCase(fetchUsers.rejected, (state, action) => {
+        builder.addCase(fetchUsers.rejected, (state, action ) => {
 
         }); 
    }
