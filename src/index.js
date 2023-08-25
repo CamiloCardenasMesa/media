148d5f29e48e981b741d91faa686b982b45c6c14
Camilo <arqcamilocardenasmesa@gmail.com>
2023-08-25 15:15:42 -0500

    chore: API server setup

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const el = document.getElementById('root');
+const root = createRoot(el);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-/**
-lo que voy a usar:
-
-@faker-js/faker: librería para generar data aletoriamente 
-@reduxjs/toolkit: Redux-toolkit
-axios: lo usaré para obtener la data (Data Fetch)
-classnames: para manejar propiedades
-json-server: con este creo el servidor api, ejecutarlo, recibir peticiones, guardarlas etc.
-react-icons: librería para mostrar íconos
-*/ 
+root.render(<App />);
