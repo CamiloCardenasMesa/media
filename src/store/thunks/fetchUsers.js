ba8db2ea96f9756bbf762cd68cb8c8171652f24b
Camilo <arqcamilocardenasmesa@gmail.com>
2023-08-25 17:09:39 -0500

    feat: export the thunk from the store/index.js file

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -5,3 +5,5 @@ const fetchUsers = createAsyncThunk('users/fetch', async () => {
     const response = await axios.get('http://localhost:3005/users'); //making the request. 
     return response.data; // resoponse.data === [{id: 1, name: 'Camilo'}] 
 }); 
+
+export { fetchUsers }; 
