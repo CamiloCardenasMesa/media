7091c9004837bb0bfaa2abf3ccd2b3d53b5aba1e
Camilo <arqcamilocardenasmesa@gmail.com>
2023-08-25 12:01:45 -0500

    chore: dependencies installed

--- src/index.js
+++ src/index.js
@@ -15,3 +15,14 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/**
+lo que voy a usar:
+
+@faker-js/faker: librería para generar data aletoriamente 
+@reduxjs/toolkit: Redux-toolkit
+axios: lo usaré para obtener la data (Data Fetch)
+classnames: para manejar propiedades
+json-server: con este creo el servidor api, ejecutarlo, recibir peticiones, guardarlas etc.
+react-icons: librería para mostrar íconos
+*/ 
