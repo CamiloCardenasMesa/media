932bce05dd5c78c2259b58bd372054e7e8a05c6d
Camilo <arqcamilocardenasmesa@gmail.com>
2023-08-25 17:21:44 -0500

    feat: actions added

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,11 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../thunks/fetchUsers";
 
 const usersSlice = createSlice({
    name: "users",
    initialState: {
-    data: [],
+        data: [],
+        isLoading: false,
+        error: null,
    },
-   reducers: {}, 
+   extraReducers(builder) {
+        builder.addCase(fetchUsers.pending); //... this is an action
+        builder.addCase(fetchUsers.fulfilled); //... this is an action
+        builder.addCase(fetchUsers.rejected); //... this is an action
+   }
 });
 
 export const usersReducer = usersSlice.reducer;
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -6,4 +6,11 @@ const fetchUsers = createAsyncThunk('users/fetch', async () => {
     return response.data; // resoponse.data === [{id: 1, name: 'Camilo'}] 
 }); 
 
+/**
+ * when create "const fetchUsers" automatically added:
+ * 1) fetchUsers.pending  === 'users/fetch/pending'
+ * 2) fetchUsers.fulfilled === 'users/fetch/fulfilled'
+ * 3) fetchUsers.rejected === 'users/fetch/rejected'
+ */
+
 export { fetchUsers }; 
