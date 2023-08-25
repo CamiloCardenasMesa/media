dafc883e9d1ff92f0cd231d58b481fc36b81e10b
Camilo <arqcamilocardenasmesa@gmail.com>
2023-08-25 17:07:29 -0500

    feat: making request that will return the data to be used in the reducer

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -1,10 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const fetchUsers = createAsyncThunk('users/fetch'); //I can put any string I want. in this case 'users/fetch'
-
-/**
- * Thunk function:
- * the goal of this thunk is to fetch some data and
- * automatically have some actions dispatched for me
- * as I´m fetching the data
- */
+const fetchUsers = createAsyncThunk('users/fetch', async () => {
+    const response = await axios.get('http://localhost:3005/users'); //making the request. 
+    return response.data; // resoponse.data === [{id: 1, name: 'Camilo'}] 
+}); 
